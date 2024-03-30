94bd7b4de8102b11a35f3d1cd9c306673361d1c1
aishacmela <aishacmela@gmail.com>
2024-03-30 13:54:41 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Weather from "./Weather";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AJAX SEARCH ENGINE</h1>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
